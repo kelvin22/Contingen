97cb43fdd5e85fcbd50ef691feb002b6aa151f93
kelvin22 <kelvin22@gmail.com>
2013-02-07 23:04:53 +1100

    changed compile flag in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Contingen::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true # change back to false if possible?
 
   # Generate digests for assets URLs
   config.assets.digest = true
