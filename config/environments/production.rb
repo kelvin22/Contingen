75c93c1b3e49080ba5004e3f967273844aceeb48
kelvin22 <kelvin22@gmail.com>
2013-03-21 21:50:35 +1100

    added wwwditcher

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,8 @@ Contingen::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Redirects www to no-www (https://github.com/isabanin/www_ditcher)
+  require 'www_ditcher'
+  config.middleware.use "WwwDitcher"
 end
