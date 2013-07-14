9baa3efe75eb393c1c6b4d3ea0e4fd9c28427a11
Kelvin Lau <kelvin22@gmail.com>
2013-07-14 15:06:31 +1000

    embedly script starts from specified index, only updates those posts which have no thumb_url or title

--- script/getfromembedly.py
+++ script/getfromembedly.py
@@ -1,23 +1,37 @@
-from embedly import Embedly
+from embedly import Embedly # pip install embedly
 import pandas as pd
 
-posts = pd.read_csv('posts.csv')
+posts = pd.read_csv('posts.csv', na_filter=False, index_col='id')
 
 client = Embedly('c1df1020e9cc4e1b8abf93c088eacc3d')
 
 
-for index, row in posts.iterrows():
-  obj = client.oembed(row['link_url'])
+start_index = 1 #start from index number (1 to n)
+writeabout_text = False
 
-  if obj.type == 'photo':
-    posts.ix[index, 'thumb_url'] = obj.url
-  else:
-    posts.ix[index, 'thumb_url'] = obj.thumbnail_url
-    posts.ix[index, 'about_text'] = obj.description
+newposts = posts[start_index-1:]
+postsforupdate = newposts[(newposts['thumb_url'] == '') | (newposts['title'] == '')]
+
+for index, row in postsforupdate.iterrows():
   
-  print index
-  print posts.ix[index, 'thumb_url']
-  print posts.ix[index, 'about_text']
-  print '\n'
+  # Run against embedly if empty thumb_url or title
+  if index >= start_index:
+    obj = client.oembed(row['link_url']) #make API call - http://embed.ly/docs/embed/api/endpoints/1/oembed
 
-posts.to_csv('posts_forreview.csv', index=False, encoding='utf-8')
+    if obj.type == 'photo':
+      posts.ix[index, 'thumb_url'] = obj.url
+    else:
+      if row['thumb_url'] == '': # only write in field if empty
+        posts.ix[index, 'thumb_url'] = obj.thumbnail_url
+      if writeabout_text == True and row['about_text'] == '':
+        posts.ix[index, 'about_text'] = obj.description
+      if row['title'] == '':
+        posts.ix[index, 'title'] = obj.description 
+    
+
+    print index
+    print posts.ix[index, 'thumb_url']
+    print posts.ix[index, 'about_text']
+    print '\n'
+
+posts.to_csv('posts_forreview.csv', index=True, encoding='utf-8')
